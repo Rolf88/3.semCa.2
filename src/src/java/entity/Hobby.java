1b963498049c938e7cb9c1edca3107d2ab87be72
TryingToImprove <privat.oliver.lassen@gmail.com>
2015-10-06 09:56:34 +0200

    Use nullable(false) and add get/setter

--- src/src/java/entity/Hobby.java
+++ src/src/java/entity/Hobby.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,11 +9,16 @@ import javax.persistence.Id;
 
 @Entity
 public class Hobby implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
 
     public Long getId() {
@@ -21,6 +27,22 @@ public class Hobby implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
@@ -47,5 +69,5 @@ public class Hobby implements Serializable {
     public String toString() {
         return "entity.Hobby[ id=" + id + " ]";
     }
-    
+
 }
