c8b23c88f8cd241b43d09b67ce24a6d6be29a612
TryingToImprove <privat.oliver.lassen@gmail.com>
2015-10-06 11:22:46 +0200

    Add facade tests

--- src/test/facade/PersonFacadeTest.java
+++ src/test/facade/PersonFacadeTest.java
@@ -2,8 +2,10 @@ package facade;
 
 import entity.Address;
 import entity.CityInfo;
+import entity.Company;
 import entity.Hobby;
 import entity.Person;
+import java.util.List;
 import javax.persistence.Persistence;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -35,6 +37,52 @@ public class PersonFacadeTest {
         Address address = person.getAddress();
         assertNotNull(address);
         assertEquals("Nørregade 2", address.getStreet());
+        assertEquals("2 th", address.getAdditionalInfo());
+
+        CityInfo city = address.getCity();
+        assertNotNull(city);
+        assertEquals("København Ø", city.getCity());
+        assertEquals("2200", city.getZip());
+    }
+
+    @Test
+    public void testIsAbleToGetAListPersons() {
+        List<Person> persons = this.personFacade.getPersons();
+
+        assertNotNull(persons);
+        assertEquals(3, persons.size());
+    }
+
+    @Test
+    public void testIsAbleToGetAListPersons_ByZipcode() {
+        int zipCode = 2200;
+        List<Person> persons = this.personFacade.getPersons(zipCode);
+
+        assertNotNull(persons);
+        assertEquals(2, persons.size());
+
+        for (Person person : persons) {
+            assertNotNull(person.getAddress());
+            assertNotNull(person.getAddress().getCity());
+            assertEquals(zipCode, person.getAddress().getCity().getZip());
+        }
+    }
+
+    @Test
+    public void testIsAbleToFindACompanyByCVR() {
+        long cvr = 123456789;
+        Company company = this.personFacade.getCompany(cvr);
+
+        assertNotNull(company);
+        assertEquals("MyCompany", company.getName());
+        assertEquals(cvr, company.getCvr());
+        assertEquals("MyCompany test description", company.getDescription());
+        assertEquals(234, company.getMarketValue());
+        assertEquals(4, company.getNumEmployees());
+
+        Address address = company.getAddress();
+        assertNotNull(address);
+        assertEquals("Nørregade 3", address.getStreet());
         assertEquals("2 th", address.getAdditionalInfo());
 
         CityInfo city = address.getCity();
