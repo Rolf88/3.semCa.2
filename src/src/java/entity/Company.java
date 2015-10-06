37d73577d2f7d91a958b8b41ae7e95d834841603
TryingToImprove <privat.oliver.lassen@gmail.com>
2015-10-06 10:14:38 +0200

    Add get/set for company

--- src/src/java/entity/Company.java
+++ src/src/java/entity/Company.java
@@ -17,4 +17,45 @@ public class Company extends InfoEntity implements Serializable {
     private int numEmployees;
 
     private int marketValue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCvr() {
+        return cvr;
+    }
+
+    public void setCvr(String cvr) {
+        this.cvr = cvr;
+    }
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int numEmployees) {
+        this.numEmployees = numEmployees;
+    }
+
+    public int getMarketValue() {
+        return marketValue;
+    }
+
+    public void setMarketValue(int marketValue) {
+        this.marketValue = marketValue;
+    }
+
 }
